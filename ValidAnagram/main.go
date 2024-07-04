4b2c68d11856144ddb8f0cd78b644b1276a88035
emirhanusta <emirhan1usta@gmail.com>
2024-07-04 11:36:27 +0300

    ValidAnagram solution added

---
+++ ValidAnagram/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	result := isAnagram("anagram", "nagaram")
+	fmt.Println(result)
+}
+
+func isAnagram(s string, t string) bool {
+
+	if len(s) != len(t) {
+		return false
+	}
+
+	m := make(map[byte]int)
+
+	for i, _ := range s {
+		m[s[i]]++
+		m[t[i]]--
+	}
+
+	for _, value := range m {
+		if value != 0 {
+			return false
+		}
+	}
+	return true
+}

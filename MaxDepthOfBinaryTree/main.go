2ccd84cf5a766dfbdbe3664f27f59f32604bbc9e
emirhanusta <emirhan1usta@gmail.com>
2024-07-16 11:06:12 +0300

    MaxDepthOfBinaryTree solution added

---
+++ MaxDepthOfBinaryTree/main.go
@@ -0,0 +1,48 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	root := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val:   9,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val:   15,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Val:   7,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+	}
+	result := maxDepth(root)
+	println(result)
+}
+
+func maxDepth(root *TreeNode) int {
+	if root != nil {
+		return 1 + maxValue(maxDepth(root.Left), maxDepth(root.Right))
+	}
+
+	return -1
+}
+
+func maxValue(left int, right int) int {
+	if left > right {
+		return left
+	}
+	return right
+}
